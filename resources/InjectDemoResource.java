94abe9175fca510bf7343030413c58cd3189878c
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2019-03-03 13:39:51 +0530

    Basic Annotations

---
+++ resources/InjectDemoResource.java
@@ -0,0 +1,21 @@
+package org.akshay.messenger.resources;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectDemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+    @GET
+    @Path("annotations")
+    public String getParamUsingAnnotations(){
+        return "Test";
+    }
+
+}
